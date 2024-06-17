ea0978031408eb2f386ef1791c63dd52cbabea14
Ethan Holz <ethan.holz@thoriumworks.com>
2024-06-17 17:13:29 -0500

    refactor: move to lib package

---
+++ lib/convert.go
@@ -0,0 +1,55 @@
+package lib
+
+import (
+	"fmt"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+// struct for parsing a actions.yml file
+type Action struct {
+	Inputs  map[string]Input  `yaml:"inputs"`
+	Outputs map[string]Output `yaml:"outputs"`
+	Name    string            `yaml:"name"`
+}
+
+type Input struct {
+	Description string      `yaml:"description"`
+	Required    bool        `yaml:"required"`
+	Default     interface{} `yaml:"default,omitempty`
+}
+
+type Output struct {
+	Description string `yaml:"description"`
+}
+
+//export GenerateTable
+func GenerateTable(input []byte) string {
+	var action Action
+	var builder strings.Builder
+	yaml.Unmarshal(input, &action)
+	if action.Name != "" {
+		fmt.Fprintf(&builder, "# %s\n\n", action.Name)
+	}
+
+	builder.WriteString("## Inputs\n")
+	builder.WriteString("| Name | Description | Required | Default |\n")
+	builder.WriteString("| ---- | ----------- | -------- | ------- |\n")
+	for name, input := range action.Inputs {
+		if input.Default == nil {
+			fmt.Fprintf(&builder, "| %s | %s | %t |  |\n", name, input.Description, input.Required)
+		} else {
+			fmt.Fprintf(&builder, "| %s | %s | %t | %v |\n", name, input.Description, input.Required, input.Default)
+		}
+	}
+	builder.WriteString("\n\n")
+	builder.WriteString("## Outputs\n")
+	builder.WriteString("| Name | Description |\n")
+	builder.WriteString("| ---- | ----------- |\n")
+	for name, output := range action.Outputs {
+		fmt.Fprintf(&builder, "| %s | %s |\n", name, output.Description)
+	}
+
+	return builder.String()
+}
--- main.go
+++ main.go
@@ -4,58 +4,14 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 
-	"gopkg.in/yaml.v3"
+	"github.com/ethanholz/action-table/lib"
 )
 
-// struct for parsing a actions.yml file
-type Action struct {
-	Inputs  map[string]Input  `yaml:"inputs"`
-	Outputs map[string]Output `yaml:"outputs"`
-	Name    string            `yaml:"name"`
-}
-
-type Input struct {
-	Description string      `yaml:"description"`
-	Required    bool        `yaml:"required"`
-	Default     interface{} `yaml:"default,omitempty`
-}
-
-type Output struct {
-	Description string `yaml:"description"`
-}
-
-func generateTable(input []byte) string {
-	var action Action
-	var builder strings.Builder
-	yaml.Unmarshal(input, &action)
-
-	builder.WriteString("## Inputs\n")
-	builder.WriteString("| Name | Description | Required | Default |\n")
-	builder.WriteString("| ---- | ----------- | -------- | ------- |\n")
-	for name, input := range action.Inputs {
-		if input.Default == nil {
-			fmt.Fprintf(&builder, "| %s | %s | %t |  |\n", name, input.Description, input.Required)
-		} else {
-			fmt.Fprintf(&builder, "| %s | %s | %t | %v |\n", name, input.Description, input.Required, input.Default)
-		}
-	}
-	builder.WriteString("\n\n")
-	builder.WriteString("## Outputs\n")
-	builder.WriteString("| Name | Description |\n")
-	builder.WriteString("| ---- | ----------- |\n")
-	for name, output := range action.Outputs {
-		fmt.Fprintf(&builder, "| %s | %s |\n", name, output.Description)
-	}
-
-	return builder.String()
-}
-
 func main() {
-	// Define flags
-	// var action Action
+	// Provide a file to read
 	file := flag.String("file", "", "File to read")
+	_ = file
 	flag.Parse()
 	if *file == "" {
 		fmt.Println("Please provide a file to read")
@@ -65,6 +21,6 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	out := generateTable(actions)
+	out := lib.GenerateTable(actions)
 	print(out)
 }
